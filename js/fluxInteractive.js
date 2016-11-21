f8ddb9daedbce4323c71add0d931f5bc1ca1853c
Josh Quick <joshquick@Josh-Quicks-MacBook-Pro.local>
2016-11-21 11:30:12 -0700

    commented interactive

--- js/fluxInteractive.js
+++ js/fluxInteractive.js
@@ -1,24 +1,18 @@
-//2. Click start
-//3. Readout for years increases by 10
-//4. Readout for tree nitrogen increases by 10
-//5. Readout for soil = ((fix value/.5)-13 * 5)
-var START_YEARS = 0;
-var START_TREES = 60;
-var START_SOILNUM = 120;
-var treePool = document.getElementById("treePool");
+var START_YEARS = 0;//Initial value for years. Set as CONSTANT to prevent side-effects
+var START_TREES = 60;//Initial value for tree. Set as CONSTANT to prevent side-effects
+var START_SOILNUM = 120;//Initial value for years. Set as CONSTANT to prevent side-effects
+var treePool = document.getElementById("treePool");//Container for treePool
+var years, trees, soilNum;//Variables for reset function to change
+resetValue();//Call for reset function
 
-var years, trees, soilNum;
-resetValue();
-
-function flowMaker() {
-    var fix = document.getElementById("flowIncr").value;
-    soilNum += ((fix /.5) - 3) * 5;
-    years += 10;
-    trees += 10;
+function flowMaker() {//Button for increasing fixation
+    var fix = document.getElementById("flowIncr").value;//Points to value for options
+    soilNum += ((fix /.5) - 3) * 5;//Equation for amount of soil nitrogen using fix values
+    years += 10;//Value for year incrementer
+    trees += 10;//Value for tree incrementer
     render();
-
-    years===50 ? (document.getElementById("10years").disabled = true) : false;
-    if (treePool.style.padding === ""){
+    years===50 ? (document.getElementById("10years").disabled = true) : false;//Disables button at value 50years
+    if (treePool.style.padding === ""){//Conditionals for adding padding to tree nitrogen ****WILL REFACTOR
         treePool.style.padding = "2px"
     } else if (treePool.style.padding === "2px"){
         treePool.style.padding = "4px"
@@ -28,9 +22,8 @@ function flowMaker() {
     }
 }
 
-// example input "2px", 2
-// should return "4px"
-function adjustPixels(value, amount){
+
+function adjustPixels(value, amount){//possible solution to replace padding for tree nitorgen
     var arr = value.split(' ');
     for (var i=0; i<arr.length; i++) {
        if (isInteger(arr[i])===true) {
@@ -39,14 +32,13 @@ function adjustPixels(value, amount){
     }
 }
 
-
-function render() {//created to stop redundancy
+function render() {//created to stop redundancy in the resetValue function
     document.getElementById("tree").innerHTML = trees;
     document.getElementById("year").innerHTML = years;
     document.getElementById("soil").innerHTML = soilNum;
 }
 
-function resetValue() {
+function resetValue() {//resets value for years, trees soil and padding.
     years = START_YEARS;//constants to reset values
     trees = START_TREES;
     soilNum = START_SOILNUM;
